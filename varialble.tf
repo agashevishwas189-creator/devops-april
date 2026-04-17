cf0d3b9afd6013320c088023d07852f90323c06b
agashevishwas189-creator <agashevishwas189@gmail.com>
2026-04-17 17:15:20 +0530

    terraform-files

---
+++ varialble.tf
@@ -0,0 +1,14 @@
+variable "ami_id " {
+    type = string
+    default = "ami-0e12ffc2dd465f6e4"
+  
+}
+variable "instance_type" {
+    type = string
+    default = "t3.micro"
+}
+ variable "vpc_security_group_ids" {
+    type = string
+    default = "sg-0c78535db3865697a"
+   
+ }
