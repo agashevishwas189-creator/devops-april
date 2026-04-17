f7a81d1959ddc77aaae6f087fe7bbcc699ee9f1f
agashevishwas189-creator <agashevishwas189@gmail.com>
2026-04-17 12:31:13 +0530

    provider

--- provider.tf
+++ provider.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-    region = "ap-south-1"
-    profile = "configs"
+    region = "ap-south-1" # Change the region as per your requirement
+    profile = "configs" # Change the profile name as per your AWS CLI configuration
   
 }
